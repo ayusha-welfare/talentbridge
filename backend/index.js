e677d4a1c3b52e46a689d6210a17c433582cfff1
ayusha-welfare <ayushasharma03@gmail.com>
2026-03-25 11:56:12 +0530

    Fixed CORS for production

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://talentbridge-app-sand.vercel.app"],
     credentials: true
 }));
 
